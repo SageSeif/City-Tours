87294d995aca3297b0e8390650ffaa51ba541ff5
Seif Emam <sagetoshiro@hotmail.com>
2022-10-10 13:31:17 +0200

    Changed Main Logo

--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -1,10 +1,10 @@
 import React from 'react'
 import "./NavBar.scss";
-import logo from "./logo1.jpg";
+import logo from "./logo2.jpg";
 export default function NavBar(){
     return (
         <nav className="navbar">
-            <img src={logo} alt="city tours logo"></img>
+            <img src={logo} alt="city tours logo" width={200} height={100}></img>
             <ul className="nav-links">
                 <li>
                     <a href="/" className="nav-link">
